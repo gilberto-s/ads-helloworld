4fffeed7715fcdbfb86243db974f7e6895a791c4
Gilberto Silva <gsds23@gmail.com>
2024-06-21 21:22:21 -0300

    feat: add testing

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    print(hello_world())
+
---
+++ test_hello.py
@@ -0,0 +1,7 @@
+import pytest
+from hello import hello_world
+
+
+def test_hello_world():
+    assert hello_world() == "Hello, World!"
+
